64b267e9ef7bd60b5ccd3fdaed8c416021fc373c
Scott Rushforth <scott@spotman.net>
2013-07-03 18:42:57 -0700

    fix indentation, stub out more stuff

--- encrypt.go
+++ encrypt.go
@@ -27,9 +27,9 @@ func (self *Salmon) generateMessageString() {
   encoding  := []byte(self.Encoding)
   algorithm := []byte(self.Algorithm)
   self.MessageString = self.EncodedPayload + 
-                       "." + base64.URLEncoding.EncodeToString(data_type) +
-                       "." + base64.URLEncoding.EncodeToString(encoding) +
-                       "." + base64.URLEncoding.EncodeToString(algorithm)
+  "." + base64.URLEncoding.EncodeToString(data_type) +
+  "." + base64.URLEncoding.EncodeToString(encoding) +
+  "." + base64.URLEncoding.EncodeToString(algorithm)
 }
 
 // populate variables post initialize
@@ -51,14 +51,14 @@ func (self *Salmon) Encrypt() {
 
   p, _ := pem.Decode([]byte(self.RSAKey))
   if p == nil {
-      panic("could not parse private key")
+    panic("could not parse private key")
   }
   key, err := x509.ParsePKCS1PrivateKey(p.Bytes)
   h := sha1.New()
   sum := h.Sum(nil)
   sig, err := rsa.SignPKCS1v15(rand.Reader, key, crypto.SHA1, sum)
   if err != nil {
-        panic("could not sign")
+    panic("could not sign")
   }
   self.Signature = base64.URLEncoding.EncodeToString(sig)
 }
@@ -66,20 +66,21 @@ func (self *Salmon) Encrypt() {
 
 // returns string of xml
 func (self *Salmon) EncryptToXml() string {
-
   self.Encrypt()
 
   template := `<?xml version='1.0' encoding='UTF-8'?>
   <entry xmlns='http://www.w3.org/2005/Atom'>
-   $encryption_header
-    <me:env xmlns:me="http://salmon-protocol.org/ns/magic-env">
-    <me:encoding>$encoding</me:encoding>
-    <me:alg>$algo</me:alg>
-    <me:data type="$datatype">$data</me:data>
-    <me:sig>$sig</me:sig>
-    </me:env>
+  $encryption_header
+  <me:env xmlns:me="http://salmon-protocol.org/ns/magic-env">
+  <me:encoding>$encoding</me:encoding>
+  <me:alg>$algo</me:alg>
+  <me:data type="$datatype">$data</me:data>
+  <me:sig>$sig</me:sig>
+  </me:env>
   </entry>`
 
+  // XXX: is there a more efficient way?
+  // XXX: encryption header is for diaspora
   template = strings.Replace(template, "$encryption_header", "", 1)
   template = strings.Replace(template, "$encoding", self.Encoding, 1)
   template = strings.Replace(template, "$algo", self.Algorithm, 1)
@@ -93,3 +94,11 @@ func (self *Salmon) EncryptToXml() string {
 // populates self from xml
 func (self *Salmon) DecryptFromXml(xml string) {
 }
+
+// is this salmon envelope valid?
+// this assumes you have populated
+// RSAKey with the key on the verifiers side
+func (self *Salmon) IsValid() bool {
+  return false
+}
+
